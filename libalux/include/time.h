c8209b7614d75271f440ba60baebda4af6451d6b
Alex Nichol <unixpickle@gmail.com>
2014-08-27 18:34:59 -0400

    profile memory; no silly C bindings

--- libalux/include/time.h
+++
@@ -1,1 +0,0 @@
-#include "../src/bindings/time.h"
--- profiler/src/memory.cpp
+++ profiler/src/memory.cpp
@@ -1,14 +1,33 @@
 #include <libalux/memory>
 #include <libalux/print>
 #include <libalux/time>
+#include <cassert>
 
 using namespace libalux;
 
 void ProfileMemoryStats() {
-  PrintLine("ProfileMemoryStats() - run at ", GetBootNanoTime() / 1000000,
-            " millis after boot");
+  uint64_t start = GetBootNanoTime();
+  
+  for (int i = 0; i < 1000000; ++i) {
+    int count = CountPageSizes();
+    for (int j = 0; j < count; ++j) {
+      GetPageSize(j);
+      GetPageAlign(j);
+    }
+    GetVMCapabilities();
+  }
+  
+  PrintLine("ProfileMemoryStats() - ", (GetBootNanoTime() - start), " nanos");
 }
 
 void ProfilePhysicalAllocation() {
-  
+  uint64_t start = GetBootNanoTime();
+  for (int i = 0; i < 20000; ++i) {
+    PhysAddr addr;
+    bool res = AllocPhysical(addr, 0x1000, 0x1000);
+    assert(res);
+    FreePhysical(addr);
+  }
+  PrintLine("ProfilePhysicalAllocation() - ", (GetBootNanoTime() - start),
+            " nanos");
 }
